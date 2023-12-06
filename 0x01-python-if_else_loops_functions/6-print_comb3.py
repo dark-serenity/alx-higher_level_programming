42b3ab0c68acb532e155e67d06c76646a887afd3
Lethu <gugulethumzamani@gmail.com>
2023-12-06 14:15:36 -0800

    print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,10 @@
-for i in range(90):
-    if i == 10:
-        continue
+#!/usr/bin/python3
 
-    print("{:02d}".format(i), end=", " if i < 89 else "")
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i >= j:
+            continue
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=", ")
