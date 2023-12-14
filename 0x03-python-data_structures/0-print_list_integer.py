baee4ceff89ee1700c9c8c20fc06c27bb1977a25
Lethu <gugulethumzamani@gmail.com>
2023-12-13 22:47:02 -0800

    print

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,18 +1,7 @@
-#!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
-    my_list = [1, 2, 3, 4, 5]
+    for number in my_list:
+        print("{:d}".format(number))
 
-# Assign individual elements from the list to variables
-first_element = my_list[0]
-second_element = my_list[1]
-third_element = my_list[2]
-fourth_element = my_list[3]
-fifth_element = my_list[4]
-
-# Print the formatted values of the first five elements
-print("{:d}".format(first_element))
-print("{:d}".format(second_element))
-print("{:d}".format(third_element))
-print("{:d}".format(fourth_element))
-print("{:d}".format(fifth_element))
+# Test the function with the provided example
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
