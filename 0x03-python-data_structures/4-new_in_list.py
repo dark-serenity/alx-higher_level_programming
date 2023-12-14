5896122b4d7b56be6467f074ec4fb29ae56120f5
Lethu <gugulethumzamani@gmail.com>
2023-12-13 23:52:01 -0800

    new

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return my_list.copy()  # Return a copy of the original list if index is out of bounds
-    my_list[idx] = element
-    return my_list.copy()  # Return a copy of the modified list
+        return my_list.copy()
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
